8c420e192023bb6bfcca28458d7a066c66149fb5
stryder03 <stryder03@users.noreply.github.com>
2020-02-12 12:58:39 -0700

    removed console messages

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -73,7 +73,6 @@ export default function ContactForm(props) {
             submitSuccess = await submitForm(data)
         }
         catch(err){
-            console.error(err);
             return false
         }
         if (submitSuccess){
