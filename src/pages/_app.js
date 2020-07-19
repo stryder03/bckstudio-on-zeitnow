9960ed4b636239c2b8b84756c3a2d3bb8175b747
stryder03 <stryder03@users.noreply.github.com>
2020-07-19 02:59:06 -0600

    auth0 mvp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ import theme from "../assets/theme";
 import PageChange from "../components/PageChange/PageChange";
 import {ParallaxProvider} from "react-scroll-parallax/cjs";
 import * as Sentry from "@sentry/react";
+import {Auth0Provider} from "@auth0/auth0-react";
 
 if (process.env.VERCEL_GITHUB_COMMIT_SHA) {
     Sentry.init({dsn: process.env.NEXT_PUBLIC_SENTRY_DSN, release: process.env.VERCEL_GITHUB_COMMIT_SHA,
@@ -43,7 +44,13 @@ export default class MyApp extends App {
           </Head>
         <ThemeProvider theme={theme}>
             <ParallaxProvider>
-                <Component {...pageProps} err={err} Sentry={Sentry}/>
+                <Auth0Provider
+                    domain="bckstudio.us.auth0.com"
+                    clientId="wHWyEFH3qroxZ5mhCRiYzj9I2XtQZ2ys"
+                    redirectUri={"http://localhost:3000"}
+                >
+                    <Component {...pageProps} err={err} Sentry={Sentry}/>
+                </Auth0Provider>
             </ParallaxProvider>
         </ThemeProvider>
       </React.Fragment>
