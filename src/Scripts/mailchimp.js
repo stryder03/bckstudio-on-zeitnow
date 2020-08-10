dabe4eb5b334ac12f5236e1a185fce664d858acf
stryder03 <stryder03@users.noreply.github.com>
2020-08-10 14:16:55 -0600

    fix: remove empty file

--- src/Scripts/mailchimp.js
+++
@@ -1,8 +0,0 @@
-export const mailchimpPopup = (c,h,i,m,p) => {
-    m=c.createElement(h),
-        p=c.getElementsByTagName(h)[0],
-        m.async=1,
-        m.src=i,
-        p.parentNode.insertBefore(m,p)
-};
-
