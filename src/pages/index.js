98f6e75aebeb9df2652f3deac64e17e021899245
stryder03 <stryder03@users.noreply.github.com>
2020-08-10 11:03:52 -0600

    feat(mailchimp): added mailchimp popup

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ export default function homePage() {
                       content="Whether you are interested in a first clay encounter, looking for the perfect pottery
                       class, or need your own ceramic studio space, Bozeman Community Kiln is here."
                 />
+                <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/7c2ebc786b5f2387d9c8b05be/7e7fb5424ce4049d3d5720ef9.js");</script>
             </Head>
             <Layout>
                 <BrandedHeader>
