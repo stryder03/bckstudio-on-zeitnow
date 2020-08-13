daff06e4d9b3290bec89a31eb65f9304baecfeb5
stryder03 <stryder03@users.noreply.github.com>
2020-08-13 01:51:54 -0600

    fix(api): added preview warning and exit button

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -1,15 +1,21 @@
-
 // route and params to call: /api/preview?token=7634b554961c40378a8769fe67194f4c&page=
+
 export default async (req, res) => {
     // token can be checked into VCS, only gives read access to draft version of cms
-    if (req.query.token !== process.env.USER_API_TOKEN) {
-        return res.status(401).json({ message: "Invalid token" })
+    if (req.query.token) {
+        if (req.query.token === process.env.USER_API_TOKEN) {
+            // Enable Preview Mode by setting the cookies
+            res.setPreviewData({token: process.env.GRAPH_CMS_PREVIEW_TOKEN_SERVER}, {});
+        } else {
+            return res.status(401).json({message: "Invalid token"})
+        }
     }
-    // Enable Preview Mode by setting the cookies
-    res.setPreviewData({token: process.env.GRAPH_CMS_PREVIEW_TOKEN_SERVER}, {
-    });
+    if (req.query.clear){
+        res.clearPreviewData();
+    }
 
-    // Redirect to the classes-encounters page
-    res.writeHead(307, { Location: "/" + req.query.page });
+    const location = "\/" + req.query.page;
+    // Redirect to page
+    res.writeHead(307, { Location: location});
     res.end()
 }
