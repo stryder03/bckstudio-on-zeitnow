8ecb9113b27915972ab17c0c42f4945f617f7693
stryder03 <stryder03@users.noreply.github.com>
2021-07-08 11:54:26 -0600

    feat(Banner): added a Banner for special events

--- src/pages-sections/Page-Sections/HeaderSection.js
+++ src/pages-sections/Page-Sections/HeaderSection.js
@@ -2,15 +2,16 @@ import React from "react";
 import LargeHeader from "../../components/Header/LargeHeader";
 import HeaderLinks from "../../components/Header/HeaderLinks";
 import MainLogo from "../../components/MainLogo/MainLogo";
-import {Hidden} from "@material-ui/core";
+import { Hidden, Typography } from "@material-ui/core";
 import SmallHeader from "../../components/Header/SmallHeader";
 import Image from "next/image";
 import { createStyles, makeStyles} from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const useStyles = makeStyles(() => createStyles({
     banner: {
         maxWidth: "1442px",
-        margin: "auto"
+        margin: "auto",
     },
     lgLogo: {
         marginTop: "-20rem",
@@ -19,6 +20,18 @@ const useStyles = makeStyles(() => createStyles({
     mdLogo: {
         marginTop: "-15rem",
         marginBottom: "5rem"
+    },
+    bannerAd: {
+        backgroundColor: "white",
+        width: "100vw",
+        marginTop: "4.5rem",
+        position: "absolute",
+        zIndex: "1",
+    },
+    text: {
+        margin: "auto",
+        align: "center",
+        justify: "center"
     }
 }))
 
@@ -61,6 +74,9 @@ export default function HeaderSection(props) {
                     {...rest}
                 />
             </Hidden>
+            <Hidden smDown>
+                <Toolbar className={classes.bannerAd}><Typography variant={"h4"} className={classes.text}>We'll Be on the Emerson Lawn for SLAM! August 7th 11am-7pm</Typography></Toolbar>
+                </Hidden>
             <div className={classes.banner}>
                 <Hidden smDown>
                     <Image src={"https://media.graphcms.com/0dKLkonoS7Kw0RXSJhRV"} width={"1442"} height={"450"} priority/>
